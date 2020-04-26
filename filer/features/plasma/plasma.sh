a52a8f5e5c1740b0e398b98a8db77170403f3080
Lucas Mitrak <lucas.mitrak@gmail.com>
2020-04-26 02:49:19 -0400

    plasma works!

--- filer/features/plasma/plasma.sh
+++ filer/features/plasma/plasma.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 source config.sh
 
-emerge -u dbus
-rc-update add dbus default
 emerge -u elogind
 rc-update add elogind boot
-emerge -u polkit
-emerge -u udisks
 emerge -u eudev
 rc-update add udev sysinit
+emerge -u sys-apps/dbus
+rc-update add dbus default
+emerge -u polkit
+emerge -u udisks
 
-emerge -u plasma-desktop
+emerge -u plasma-meta
 
 if [ $user ]; then
 	usermod -a -G audio,cdrom,video,cdrw,usb,users $user
-	echo "exec dbus-launch --exit-with-session startplasma-x11">>/home/$user/.xinitrc
+	echo "exec dbus-launch --exit-with-session startplasma-x11">/home/$user/.xinitrc
 fi
